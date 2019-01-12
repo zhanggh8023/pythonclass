a43fab899383144bdea61e61f23963995c021a5e
zhanggh <zhanggh@citydo.com.cn>
2019-01-12 12:10:16 +0800

    新增图片文件夹

--- Cut_image.py
+++ Cut_image.py
@@ -52,7 +52,7 @@ def save_images(image_list):
 
 
 if __name__ == '__main__':
-    file_path = "timg.jpg"
+    file_path = ".\image\微信图片_20190112115141.jpg"
     image = Image.open(file_path)
     # image.show()
     image = fill_image(image)
