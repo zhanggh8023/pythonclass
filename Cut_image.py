0c1dd244a105128ff7292bc10e3a990e3b8da2ee
zhanggh <zhanggh@citydo.com.cn>
2019-01-16 16:04:19 +0800

    图片

--- Cut_image.py
+++ Cut_image.py
@@ -52,7 +52,7 @@ def save_images(image_list):
 
 
 if __name__ == '__main__':
-    file_path = ".\image\微信图片_20190112115141.jpg"
+    file_path = ".\image\\009.jpg"
     image = Image.open(file_path)
     # image.show()
     image = fill_image(image)
