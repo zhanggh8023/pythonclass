b15a3ae86e5952efbd6b0078b000ce2023a5389e
zhanggh <zhanggh@citydo.com.cn>
2019-02-13 15:39:32 +0800

    搭建电影列表、搜索、详情页、404页面代码

--- Flask_study/app/__init__.py
+++ Flask_study/app/__init__.py
@@ -5,8 +5,7 @@
 # @File    : __init__.py.py
 # @Software: PyCharm
 
-
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app=Flask(__name__)
 app.debug=True
@@ -19,3 +18,8 @@ from Flask_study.app.admin import admin as admin_blueprint
 # 注册蓝图 (添加前后端模块，将蓝图中的url映射关系加载到项目中)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint,url_prefix="/admin")
+
+#404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"),404
--- Flask_study/app/home/views.py
+++ Flask_study/app/home/views.py
@@ -10,9 +10,9 @@ from . import home
 from flask import Flask, render_template, request, redirect, url_for
 
 # 2. 创建蓝图的视图函数 (通过蓝图装饰路由)
-@home.route("/")
-def index():
-    return render_template("home/index.html")
+# @home.route("/")
+# def index():
+#     return render_template("home/index.html")
 
 
 # 登录
@@ -31,3 +31,53 @@ def logout():
 @home.route("/regist/")
 def regist():
     return render_template("home/regist.html")
+
+#会员
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+#密码
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+#评论
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+#登录日志
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+#电影收藏
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
+
+#电影列表
+@home.route("/")
+def index():
+    return render_template("home/index.html")
+
+#电影收藏
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+
+#搜索
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
+
+#详情
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
+
+
+
+
+
