4894ca1791fe700f4cd1ac2d5eeea8d7c0e2d63b
zhanggh <849080458@qq.com>
2019-02-19 18:08:40 +0800

    拖拽

--- price/元素拖拽实现.py
+++ price/元素拖拽实现.py
@@ -19,15 +19,15 @@ button='//div[@class="landing_form_button"]//span'
 
 button_1='//div[@id="modelManage"]//button[@class="ant-btn addModel"]'#新建模型
 
-button_2='//*[@class="form-group"]/*[@class="form-control"]'#组件
-button_3='//*[@class="form-group mt-20"]/*[@class="btn btn-special"]'#画布
+button_2='//div[@class="diagram-operation-wrap"]//div[@data-id="1"]/img'#过滤
+button_3='//div[@id="model-svg-wrapper"]/*'#画布
 #等待账号输入框元素出现
 WebDriverWait(driver,10,0.5).until(EC.visibility_of_all_elements_located((By.XPATH,name)))
 #输入手机号
-driver.find_element_by_xpath(name).send_keys('zgh')
+driver.find_element_by_xpath(name).send_keys('admin')
 time.sleep(1)
 #输入密码
-driver.find_element_by_xpath(pwd).send_keys('111111111')
+driver.find_element_by_xpath(pwd).send_keys('123456')
 #等待登录按钮元素出现
 WebDriverWait(driver,10,0.5).until(EC.visibility_of_all_elements_located((By.XPATH,button)))
 #点击登录
@@ -40,18 +40,30 @@ WebDriverWait(driver,10,0.5).until(EC.visibility_of_all_elements_located((By.XPA
 driver.find_element_by_xpath(button_1).click()
 
 #等待登录按钮元素出现
-WebDriverWait(driver,10,0.5).until(EC.visibility_of_all_elements_located((By.XPATH,button_1)))
-#拖拽组件
-# 起点
-start = driver.find_element_by_id("draggable")
-# 终点
-end = driver.find_element_by_id("droppable")
+# WebDriverWait(driver,10,0.5).until(EC.visibility_of_all_elements_located((By.XPATH,button_2)))
+# driver.find_element_by_xpath(button_2).click()
+# # move_to  移动
+# # 定位到元素的源位置
+# ele = driver.find_element_by_link_text(button_2)
+# # 将鼠标移动到定位的元素上面
+# ActionChains(driver).move_to_element(ele).perform()
+#
+# #拖拽组件
+# # 起点
+# start = driver.find_element_by_id(button_2)
+# # 终点
+# end = driver.find_element_by_id(button_3)
+#
+# actions = ActionChains(driver)
+# actions.drag_and_drop(start, end)
+# # 执行
+# actions.perform()
 
-actions = ActionChains(driver)
-actions.drag_and_drop(start, end)
-# 执行
-actions.perform()
-
+#获取模型ID
+model_name_xpath='//div[@id="model-svg-wrapper"]//div[@class="model-name"]'
+WebDriverWait(driver,10,0.5).until(EC.visibility_of_all_elements_located((By.XPATH,model_name_xpath)))
+edddd=driver.find_element_by_xpath(model_name_xpath).text()
+print(edddd)
 
 
 
