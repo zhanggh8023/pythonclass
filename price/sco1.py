150f69507fde03b59035bbc55e92f1bf69e9dc8b
zhanggh <849080458@qq.com>
2019-02-20 18:15:34 +0800

    元素拖拽方法

--- price/sco1.py
+++ price/sco1.py
@@ -5,16 +5,36 @@
 # @File    : sco1.py
 # @Software: PyCharm
 
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+import time
 
-m = int(input())
-n = int(input())
-c = list(map(int,input().split()))
-dp = [100000]*(m+1)
-dp[0] = 0
-for i in range(n):
-    for j in range(m,c[i]-1,-1):
-        dp[j]=min(dp[j],dp[j-c[i]]+1)
-if dp[m]>=100000:
-    print(0)
-else:
-    print(dp[m])
+browser = webdriver.Chrome()
+try:
+    browser.get("http://www.runoob.com/try/try.php?filename=jqueryui-api-droppable")
+    browser.maximize_window()
+
+    browser.switch_to.frame('iframeResult')
+
+    source = browser.find_element_by_id('draggable')
+    target = browser.find_element_by_id('droppable')
+
+    print(source)
+    print(target)
+    actions = ActionChains(browser)
+    actions.drag_and_drop(source, target)
+    # 执行动作
+    actions.perform()
+
+    # 关闭提示框
+    alert = browser.switch_to.alert
+    print(alert)
+    time.sleep(3)
+    alert.dismiss()
+
+    time.sleep(5)
+except Exception as e:
+    print(e)
+finally:
+    browser.quit()
+    print('OK')
