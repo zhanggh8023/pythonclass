2a2db02f3cf7eb9b1bfeb128acf84d7c8a530f58
zgh <zhanggh@citydo.com.cn>
2019-05-27 16:28:16 +0800

    pycharm索引

--- opendoor/openDoor.py
+++ opendoor/openDoor.py
@@ -8,7 +8,8 @@
 import requests
 
 url="https://zlxprod.citydo.com.cn/menjin/attendance/openDoor"
-data={'phone':17681829051,'wifiName':'Citydo','wifiLevel':5,'wifiMac':'48:bd:3d:d9:c6:40','token':'eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.HFWtJ9rXvo6BrAwKyEzmWLTxcXfiMIejd55epKAwMjMmkihkPrrvIg.9kVBEDLjt8-VO_1DGxaelA.YBEBfhR1lOe1D9mUQEGvKH69SHFV0qtl0zaDCBNBxRhIslsYCR2uRLyJBSPFiW3wkog1oPeG_pOUePeggL5k2w.J6yIuTKbLI05tfoMBfXC8g'}
+data={'phone':17681829051,'wifiName':'Citydo','wifiLevel':5,'wifiMac':'48:bd:3d:d9:c6:40',
+      'token':'eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.HFWtJ9rXvo6BrAwKyEzmWLTxcXfiMIejd55epKAwMjMmkihkPrrvIg.9kVBEDLjt8-VO_1DGxaelA.YBEBfhR1lOe1D9mUQEGvKH69SHFV0qtl0zaDCBNBxRhIslsYCR2uRLyJBSPFiW3wkog1oPeG_pOUePeggL5k2w.J6yIuTKbLI05tfoMBfXC8g'}
 files={'file':open('kaimen.wav','rb')}
 res=requests.post(url,data=data,files=files)
 print(res.text)
