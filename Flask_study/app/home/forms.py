a6117518b5ad1d67dda34197f5f047a2bb47b710
zgh <zhanggh@citydo.com.cn>
2019-05-27 18:14:47 +0800

    更新会员注册模块

--- Flask_study/app/home/forms.py
+++ Flask_study/app/home/forms.py
@@ -4,3 +4,101 @@
 # @Email   : 849080458@qq.com
 # @File    : forms.py
 # @Software: PyCharm
+
+from flask_wtf import FlaskForm
+from wtforms.fields import SubmitField, StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo, Email, Regexp,ValidationError
+from Flask_study.app.models import User
+
+class RegistForm(FlaskForm):
+    name = StringField(
+        label="昵称",
+        validators=[
+            DataRequired("请输入昵称!!!")
+        ],
+        description="昵称",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "请输入昵称!",
+        }
+    )
+    email = StringField(
+        label="邮箱",
+        validators=[
+            DataRequired("请输入邮箱!!!"),
+            Email('邮箱格式不正确')
+        ],
+        description="邮箱",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "请输入邮箱!",
+        }
+    )
+    phone = StringField(
+        label="手机",
+        validators=[
+            DataRequired("请输入手机!!!"),
+            Regexp('^1[3|4|5|6|7|8][0-9]\d{4,8}$', message='手机格式不正确!!!')
+        ],
+        description="手机",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "请输入手机!",
+        }
+    )
+    pwd = PasswordField(
+        label="密码",
+        validators=[
+            DataRequired("请输入密码!!!")
+        ],
+        description="密码",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "请输入密码!",
+            # "required": "required"
+        }
+    )
+    repwd = PasswordField(
+        label="确认密码",
+        validators=[
+            DataRequired("请输入确认密码!!!"),
+            EqualTo('pwd', message='两次密码不一致！！')
+        ],
+        description="确认密码",
+        render_kw={
+            "class": "form-control input-lg",
+            "placeholder": "请输入确认密码!",
+            # "required": "required"
+        }
+    )
+    submit = SubmitField(
+        '注册',
+        render_kw={
+            "class": "btn btn-lg btn-success btn-block",
+        }
+    )
+
+    def validata_name(self,field):
+        name=field.data
+        user=User.query.filter_by(name=name).count()
+        if user==1:
+            raise ValidationError("昵称已经存在！！！")
+
+    def validata_email(self,field):
+        email=field.data
+        user=User.query.filter_by(email=email).count()
+        if user==1:
+            raise ValidationError("邮箱已经存在！！！")
+
+    def validata_phone(self, field):
+        phone = field.data
+        user = User.query.filter_by(phone=phone).count()
+        if user == 1:
+            raise ValidationError("手机号码已经存在！！！")
+
+
+
+
+
+
+
--- Flask_study/app/home/views.py
+++ Flask_study/app/home/views.py
@@ -7,7 +7,13 @@
 
 
 from . import home
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from Flask_study.app.home.forms import RegistForm
+from Flask_study.app.models import User
+from werkzeug.security import generate_password_hash
+from Flask_study.app import db
+import uuid
+
 
 # 2. 创建蓝图的视图函数 (通过蓝图装饰路由)
 # @home.route("/")
@@ -21,63 +27,80 @@ def login():
     return render_template("home/login.html")
 
 
-#退出
+# 退出
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home.login"))
 
 
 # 注册
-@home.route("/regist/")
+@home.route("/regist/", methods=["GET", "POST"])
 def regist():
-    return render_template("home/regist.html")
+    form = RegistForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User(
+            name=data['name'],
+            email=data['email'],
+            phone=data['phone'],
+            pwd=generate_password_hash(data['pwd']),
+            uuid=uuid.uuid4().hex
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("注册成功！！", "ok")
+    return render_template("home/regist.html", form=form)
 
-#会员
+
+# 会员
 @home.route("/user/")
 def user():
     return render_template("home/user.html")
 
-#密码
+
+# 密码
 @home.route("/pwd/")
 def pwd():
     return render_template("home/pwd.html")
 
-#评论
+
+# 评论
 @home.route("/comments/")
 def comments():
     return render_template("home/comments.html")
 
-#登录日志
+
+# 登录日志
 @home.route("/loginlog/")
 def loginlog():
     return render_template("home/loginlog.html")
 
-#电影收藏
+
+# 电影收藏
 @home.route("/moviecol/")
 def moviecol():
     return render_template("home/moviecol.html")
 
-#电影列表
+
+# 电影列表
 @home.route("/")
 def index():
     return render_template("home/index.html")
 
-#电影收藏
+
+# 电影收藏
 @home.route("/animation/")
 def animation():
     return render_template("home/animation.html")
 
-#搜索
+
+# 搜索
 @home.route("/search/")
 def search():
     return render_template("home/search.html")
 
-#详情
+
+# 详情
 @home.route("/play/")
 def play():
     return render_template("home/play.html")
-
-
-
-
-
