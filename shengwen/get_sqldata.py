f031b12be85fddb74de5d85c84a8dd39e5ffe84c
zgh <zhanggh@citydo.com.cn>
2019-06-11 13:08:21 +0800

    修改记录执行次数方法

--- shengwen/get_sqldata.py
+++ shengwen/get_sqldata.py
@@ -8,43 +8,39 @@
 
 import mysql.connector
 
+# 连接数据库
+# 1、数据库连接信息
+config = {'host': '47.97.253.39',
+          'user': 'root',
+          'password': 'cityDo@123',
+          'port': 3306,
+          'database': 'aqhy',
+          }
 
-#连接数据库
-#1、数据库连接信息
-config={'host':'47.97.253.39',
-        'user':'root',
-        'password':'cityDo@123',
-        'port':3306,
-        'database':'aqhy',
-        }
+
 def get_data():
-    #2、登录数据库
-    connector=mysql.connector.connect(**config)#  **后面加上配置文件名
+    # 2、登录数据库
+    connector = mysql.connector.connect(**config)  # **后面加上配置文件名
 
+    # 建游标
+    cursor = connector.cursor(buffered=True)
 
-    #建游标
-    cursor=connector.cursor(buffered=True)
-
-    list_1={}
+    list_1 = {}
 
     cur = cursor.execute("select name,phone from aqhy.access_user")
 
-    #查看数据库数据
+    # 查看数据库数据
     desc = cursor.description  # 获取字段的描述，默认获取数据库字段名称，重新定义时通过AS关键重新命名即可
     data_dict = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]  # 列表表达式把数据组装起来
-
 
     print(data_dict)
     return data_dict
 
-
-    #切记一定要执行
+    # 切记一定要执行
     cursor.execute('commit')
 
-
-    #关闭游标
+    # 关闭游标
     cursor.close()
 
-    #关闭连接
+    # 关闭连接
     connector.close()
-
--- shengwen/voiceprint_recognition_auto.py
+++ shengwen/voiceprint_recognition_auto.py
@@ -7,16 +7,17 @@
 
 import requests
 import json
+
 requests.packages.urllib3.disable_warnings()
 import os
-from shengwen.RWexcel import readexcel, write_Excel,write_sqldata
+from shengwen.RWexcel import readexcel, write_Excel, write_sqldata
 from shengwen.get_sqldata import get_data
 
+# 获取数据库姓名手机号，写入excel
+write_sqldata('voicedata.xlsx', 'Sheet1', len(get_data()), get_data())
 
-#获取数据库姓名手机号，写入excel
-write_sqldata('voicedata.xlsx','Sheet1',len(get_data()),get_data())
 
-#获取目录下语音名称
+# 获取目录下语音名称
 def file_name(file_dir):
     for root, dirs, files in os.walk(file_dir):
         print('root_dir:', root)  # 当前目录路径
@@ -25,21 +26,22 @@ def file_name(file_dir):
     return files
 
 
-
 name = file_name('./yuying/')
 print(len(name))
 
 datas = readexcel('Sheet1')
 url = "http://47.96.120.226:8180/menjin/voice/voiceLogin"
-n=0
+n = 0
+m = 0
+k = 0
 for ii in range(len(datas)):
     sorce = []
     for i in range(len(datas)):
-        path = './yuying/' + datas[i]['phone'] + '6.wav'
-        filename=datas[i]['phone'] + '6.wav'
+        path = './yuying/' + str(datas[i]['phone']) + '6.wav'
+        filename = datas[i]['phone'] + '6.wav'
         print(filename)
-        print(datas[ii]['name'],'→',datas[ii]['phone'],'→',path)
-        if os.path.exists('./yuying/' + datas[i]['phone'] + '6.wav')==False:
+        print(datas[ii]['name'], '→', datas[ii]['phone'], '→', path)
+        if os.path.exists('./yuying/' + datas[i]['phone'] + '6.wav') == False:
             sorce.append('语音不存在')
             print('语音不存在')
             continue
@@ -47,16 +49,19 @@ for ii in range(len(datas)):
         data = {'phone': datas[ii]['phone'],
                 'token': 'eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.HFWtJ9rXvo6BrAwKyEzmWLTxcXfiMIejd55epKAwMjMmkihkPrrvIg.9kVBEDLjt8-VO_1DGxaelA.YBEBfhR1lOe1D9mUQEGvKH69SHFV0qtl0zaDCBNBxRhIslsYCR2uRLyJBSPFiW3wkog1oPeG_pOUePeggL5k2w.J6yIuTKbLI05tfoMBfXC8g'}
         res = requests.post(url, data=data, files=files, verify=False)
-        dict=json.loads(res.text)
+        dict = json.loads(res.text)
         print(dict)
-        if dict['code']==0:
+        if dict['code'] == 0:
             print('成功')
-            sorce.append('成功：'+dict['msg'])
+            sorce.append('成功：' + dict['msg'])
+            m += 1
         else:
             sorce.append(dict['data'])
-            print('加入分数：',dict['data'])
-        n +=1
-        print(n)
+            print('加入分数：', dict['data'])
+            k += 1
+        n += 1
+        print('对比次数：' + n, '通过次数：' + m, '未通过次数：' + k)
 
-    write_Excel('voicedata.xlsx','Sheet1',ii+2,sorce,len(datas))
+    write_Excel('voicedata.xlsx', 'Sheet1', ii + 2, sorce, len(datas))
+
 
